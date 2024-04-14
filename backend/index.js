b5bd4f9fe25524b5255772ceacc717ef9810d494
Siraj-Ahamed <ahamedsiraj770@gmail.com>
2024-04-14 06:39:38 +0530

    db connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require('dotenv')
 
-app.listen(3000, () => {
-    console.log("Server is lisening on port 3000");
-});
+dotenv.config()
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected to Database.");
+        app.listen(3000, () => {
+            console.log("Server is lisening on port 3000");
+        });
+    })
+    .catch((err) => {
+        console.log("Connecion failed");
+        console.log("Connecion ERROR:", err);
+    });
